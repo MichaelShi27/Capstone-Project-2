1b7558828ceca23ccef7324949e96ddff4b7d7e8
Michael Shi <michaelshi03@gmail.com>
2023-05-19 14:46:10 -0700

    Display park info on click

--- js/parks.js
+++ js/parks.js
@@ -2,11 +2,9 @@ import { parkTypes } from './data/parkTypeData.js';
 import { parks } from './data/nationalParkData.js';
 import { locations } from './data/locationData.js';
 
-import { qS, qSA, setDisplay, hideElements } from './helpers.js';
+import { qS, qSA, setDisplay } from './helpers.js';
 
 const parkSelect = qS('#parks-select');
-parkSelect.size = 10;
-
 const parkList = qS('#parks-list');
 
 window.onload = () => {
@@ -37,15 +35,43 @@ const displayParks = searchType => {
   const filteringFunc = ({ state, name }) => searchType === 'Location' ? state === selected : name.includes(selected);
   const selectedParks = parks.filter(filteringFunc);
 
-  for (const { name } of selectedParks) {
+  for (const selectedPark of selectedParks) {
+    const { name, city, state } = selectedPark;
+
     const parkLi = document.createElement('li');
-    parkLi.textContent = name;
     parkList.appendChild(parkLi);
-    // parkLi.onclick = () => displayParkInfo(parkLi);
+    parkLi.innerHTML = `
+      <div class="d-flex justify-content-between">
+        <span>${name}</span>
+        <span>${city}, ${state}</span>
+      </div>
+    `;
+
+    parkLi.onclick = () => displayParkInfo(parkLi, selectedPark);
   }
 };
 
-// const displayParkInfo = parkLi => {
-//   console.log('yo')
-//   parkLi.innerHTML = '<h1>hi</h1>';
-// };
+const displayParkInfo = (parkLi, selectedPark) => {
+  const { id, name, address, city, state, zipCode, phone, fax } = selectedPark;
+
+  if ( qS(`button#${id}-close`) ) return; // if a button already exists for this park
+
+  const contact = phone || (fax ? `Fax: ${fax}` : '');
+  parkLi.innerHTML = `
+    <div class="selectedPark">
+      <div>${name}</div>
+      <span class="m-3">${address}</span>
+      <span class="m-3">${city}</span>
+      <span class="m-3">${state}</span>
+      <span class="m-3">${zipCode}</span>
+      <div>${contact}</div>
+    </div>
+  `;
+  const btnStr = `<button id="${id}-close" class="close-btn">Close</button>`;
+  parkLi.insertAdjacentHTML('afterend', btnStr);
+  
+  qS(`button#${id}-close`).onclick = function() {
+    parkLi.innerHTML = name;
+    this.remove();
+  };
+};
