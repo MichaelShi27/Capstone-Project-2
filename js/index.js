54d5c8093bee1d2700444c1a87df9e15be1f17f9
Michael Shi <michaelshi03@gmail.com>
2023-05-23 16:28:24 -0700

    Finish hover functionality for home page

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 import { qS } from './helpers.js';
 
+const heading = qS('#heading');
+const sky = qS('#sky');
 const park1 = qS('#park1');
 const park2 = qS('#park2');
+const mountain = qS('#mountain');
 
-park1.onmouseenter = () => park2.classList.add('hover');
-park2.onmouseenter = () => park1.classList.add('hover');
+const rewriteHeading = type => heading.innerHTML = `<h4>Search ${type}?</h4>`;
 
-park1.onmouseleave = () => park2.classList.remove('hover');
-park2.onmouseleave = () => park1.classList.remove('hover');
+sky.onmouseenter = () => heading.innerHTML = 'Outsidë';
+mountain.onmouseenter = () => rewriteHeading('mountains');
+
+park1.onmouseenter = () => parkMouseHelper(park2, 'add', 'parks');
+park1.onmouseleave = () => parkMouseHelper(park2, 'remove');
+
+park2.onmouseenter = () => parkMouseHelper(park1, 'add', 'parks');
+park2.onmouseleave = () => parkMouseHelper(park2, 'remove');
+
+const parkMouseHelper = (element, funcStr, searchType) => {
+  element.classList[funcStr]('hover'); // add or remove 'hover' from classList
+  if (searchType) rewriteHeading(searchType);
+};
