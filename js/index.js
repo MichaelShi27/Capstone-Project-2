178b7eb147e957030ac5ece3e5fba89f98b1388d
Michael Shi <michaelshi03@gmail.com>
2023-05-25 00:15:50 -0700

    Alter styling for home page

--- js/index.js
+++ js/index.js
@@ -2,22 +2,11 @@ import { qS } from './helpers.js';
 
 const heading = qS('#heading');
 const sky = qS('#sky');
-const park1 = qS('#park1');
-const park2 = qS('#park2');
 const mountain = qS('#mountain');
+const park = qS('#park');
 
-const rewriteHeading = type => heading.innerHTML = `<h4>Search ${type}?</h4>`;
+const rewriteHeading = str => heading.innerHTML = str ? `<h4>Search ${str}?</h4>` : 'Outsidë';
 
-sky.onmouseenter = () => heading.innerHTML = 'Outsidë';
+sky.onmouseenter = () => rewriteHeading('Outsidë');
 mountain.onmouseenter = () => rewriteHeading('mountains');
-
-park1.onmouseenter = () => parkMouseHelper(park2, 'add', 'parks');
-park1.onmouseleave = () => parkMouseHelper(park2, 'remove');
-
-park2.onmouseenter = () => parkMouseHelper(park1, 'add', 'parks');
-park2.onmouseleave = () => parkMouseHelper(park1, 'remove');
-
-const parkMouseHelper = (element, funcStr, searchType) => {
-  element.classList[funcStr]('hover'); // add or remove 'hover' from classList
-  if (searchType) rewriteHeading(searchType);
-};
+park.onmouseenter = () => rewriteHeading('parks');
